dcbc1abd18c6704ccd54bdd47bbd85199d42f922
xakazukinx <xakazukinx.goo@gmail.com>
2019-02-22 02:42:35 +0900

    [add]Photonの入室処理(Playerを生み出してVCamのLookAtの対象に指定する)を行うスクリプトを作成

---
+++ TAMAkorogashi/Assets/Scripts/PhotonSetUp.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonSetUp : MonoBehaviour 
+{
+
+	static string GAME_VERSION = "Ver.1";
+	private int testVal = 0;
+
+	private PhotonView _photonView;
+
+	[SerializeField] private string roomName;
+	//------------------------------------------------------------------------------------------------------------------------------//
+	void Start() 
+	{
+		_photonView = GetComponent<PhotonView>();
+
+		PhotonNetwork.ConnectUsingSettings("0.0.1v");
+		// アプリの起動と同時にPhotonCloudに接続
+		Debug.Log("PhotonLogin: マスターサーバーに接続します");
+		PhotonNetwork.gameVersion = GAME_VERSION;
+
+	}
+    
+	public void OnConnectedToMaster() {
+
+		Debug.Log("PhotonLogin: マスターサーバーに接続しました");
+		Debug.Log("PhotonLogin: ルームに入室します");
+
+		// ルームへの参加 or 新規作成
+		PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions(), null);
+
+	}
+
+	public void OnJoinedRoom()
+	{
+		if (PhotonNetwork.isMasterClient)
+		{
+			Debug.Log("この端末はマスタークライアントです");
+		}
+		else
+		{
+			Debug.Log("この端末はマスタークライアントではありません");
+		}
+	}
+    
+
+	private void Update()
+	{
+		//Debug.Log(PhotonNetwork.room.Name);
+		if (PhotonNetwork.isMasterClient)
+		{
+            
+			//  Debug.Log("Master");
+		}
+
+		if (Input.GetKeyDown(KeyCode.F12))
+		{
+			if(!PhotonNetwork.isMasterClient)
+				_photonView.RPC("test",PhotonTargets.All);  
+		}
+	}
+
+	[PunRPC]
+	private void test()
+	{
+		testVal += 1;
+		Debug.Log(testVal);
+	}
+    
+}
--- TAMAkorogashi/Assets/Scripts/Player.cs
+++ TAMAkorogashi/Assets/Scripts/Player.cs
@@ -7,18 +7,26 @@ public class Player : MonoBehaviour {
 	// speedを制御する
 	[SerializeField] private float speed = 10;
 	private Rigidbody rigidbody;
+	private PhotonView _photonView;
 
 	private void Start()
 	{
+		_photonView = GetComponent<PhotonView>();
 		rigidbody = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate ()
 	{
+		
+		if (!_photonView.isMine)
+		{
+			return;
+		}
 		float x = Input.GetAxis("Horizontal");
 		float z = Input.GetAxis("Vertical");
 		
 		// xとzにspeedを掛ける
 		rigidbody.AddForce(x * speed, 0, z * speed);
+		
 	}
 }
